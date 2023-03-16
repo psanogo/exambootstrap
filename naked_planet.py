7311d6c845c76c4c120bf9720d22e91d366d6925
psanogo <97117540+psanogo@users.noreply.github.com>
2023-03-16 14:28:00 +0000

    Add files via upload

---
+++ naked_planet.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Getting Started with your Lab Sandbox</h1>
+# <p>You can find important course-specific tips and notes for your Lab Sandbox in this <br>
+#     quick guide to use throughout your course. You’ll be able to reference this at any time <br>
+#     or visit the <a href="https://learner.coursera.help/hc/articles/360062301971">Learner Help Center</a>
+#     for more info. </p>
+#     
+# <h2>What tools are already installed in my sandbox environment?</h2>
+# <ul>
+#   <li>Python 3</li>
+#   <li>R </li>
+#   <li>numpy</li>
+#   <li>matplotlib</li>
+#   <li>gfortran</li>
+#   <li>math</li>
+# </ul>
+# 
+# <h2>How can I run Python in <b>terminal</b> in the Lab Sandbox?</h2>
+# <ol>Type <b>python[version]</b> followed by your filename. For example, python3 filename.py </ol>   
+# 
+# <h2>How can I run Python in a Jupyter Notebook in the Lab Sandbox?</h2>
+# <ol>For any Python visualizations needed, you can select the Python kernel in your sandbox <br>
+#     by creating a "New" notebook with the Python 3 kernel type. </ol>
+
+# <h2>Additional Course Specific Notes:</h2>
+#     
+# <ul>
+# <br>
+#     <li><b>IMPORTANT: To optimize your notebook performance, please shut down each of your <br>
+#         open/running Jupyter Notebooks after you complete them within your Lab</b>. <br>
+#         This will provide full and dedicated system resources to the current lab assignment<br>
+#         you are working on in your Lab Sandbox. If you experience any unexpected slow <br>
+#         processing in your notebook, try to restart your Lab kernel and only open that single <br>
+#         notebook to complete your work. You can learn more about restarting your Lab Sandbox <br>
+#         and troubleshooting your lab in our Learner Help Center article <a href="https://learner.coursera.help/hc/articles/360044758731-Solving-Common-Issues-with-Coursera-Labs">here</a>.</li><br>
+#     <li>If anything is unclear regarding the course content, please go through the Discussion board and see if your question has been answered. <br>If you have any issues with the Jupyter enviornment itself, please contact Coursera through the <a href="https://learner.coursera.help">Learner Help Center.</a></li><br>
+# </ul>
+
+# In[ ]:
+
+
+nSteps = int(input(""))
+
+
+# In[ ]:
+
+
+#import matplotlib
+
+
+# In[ ]:
+
+
+print(TK[-1], heatOut)
+
+
+# In[ ]:
+
+
+python naked_planet.py
+1
+44.15624999999999 0.21555186906704765  
+
+
+# In[ ]:
+
+
+python naked_planet.py
+2
+88.2722123292339 3.442540862251764     
+
+
+# In[ ]:
+
+
+program naked_planet
+
+
+# In[ ]:
+
+
+read(5,*) nSteps
+
+
+# In[ ]:
+
+
+write(6,*) temperature, heatFlux
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
